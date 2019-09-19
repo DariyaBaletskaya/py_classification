ee02b3a8ef359cbafb63ce61628e3e27dcd98519
Illia Uzun <you@example.com>
2019-09-20 00:08:06 +0300

    lab_1 final refactor

--- сheckbox.py
+++ сheckbox.py
@@ -4,59 +4,68 @@ from matplotlib.figure import Figure
 import numpy as np
 from tkinter import *
 
+
 # ------DATA-----#
-class1 = np.array([[0.49, 0.89],
-                   [0.34, 0.81],
-                   [0.36, 0.67],
-                   [0.47, 0.49],
-                   [0.52, 0.53]])
-class3 = np.array([[0.62, 0.83],
-                   [0.79, 0.92],
-                   [0.71, 0.92],
-                   [0.78, 0.83],
-                   [0.87, 0.92]])
-class4 = np.array([[0.55, 0.4],
-                   [0.66, 0.32],
-                   [0.74, 0.49],
-                   [0.89, 0.3],
-                   [0.77, 0.02]])
-class6 = np.array([[0.05, 0.15],
-                   [0.09, 0.39],
-                   [0.13, 0.51],
-                   [0.25, 0.34],
-                   [0.15, 0.36]])
+class Data:
+    class1 = np.array([[0.49, 0.89],
+                       [0.34, 0.81],
+                       [0.36, 0.67],
+                       [0.47, 0.49],
+                       [0.52, 0.53]])
+    class3 = np.array([[0.62, 0.83],
+                       [0.79, 0.92],
+                       [0.71, 0.92],
+                       [0.78, 0.83],
+                       [0.87, 0.92]])
+    class4 = np.array([[0.55, 0.4],
+                       [0.66, 0.32],
+                       [0.74, 0.49],
+                       [0.89, 0.3],
+                       [0.77, 0.02]])
+    class6 = np.array([[0.05, 0.15],
+                       [0.09, 0.39],
+                       [0.13, 0.51],
+                       [0.25, 0.34],
+                       [0.15, 0.36]])
+    classes = [class1, class3, class4, class6]
+    naming = ["Class #1", "Class #2", "Class #3", "Class #4"]
 
 
 class Calculations:
-    def euclid_distance(self, class1, class2):
+    @staticmethod
+    def euclid_distance(class1, class2):
         dx = (class1[0] - class2[0]) ** 2
         dy = (class2[1] - class2[1]) ** 2
         d_euclid = math.sqrt(dx + dy)
         return d_euclid
 
     # 1.5
-    def max_mod_distance(self, class1, class2):
+    @staticmethod
+    def max_mod_distance(class1, class2):
         dx = math.fabs(class1[0] - class2[0])
         dy = math.fabs(class2[1] - class2[1])
         return max([dx, dy])
 
     # 2.1
-    def calculate_distance_to_centroid(self, func, ePoint, centroid):
-        d = func(ePoint, centroid)
+    @staticmethod
+    def calculate_distance_to_centroid(func, e_point, centroid):
+        d = func(e_point, centroid)
         return d
 
     # 2.1 Util
-    def find_centroid(self, eClass):
-        xMean = np.mean(eClass[:, 0])
-        yMean = np.mean(eClass[:, 1])
-        return [xMean, yMean]
+    @staticmethod
+    def find_centroid(eClass):
+        x_mean = np.mean(eClass[:, 0])
+        y_mean = np.mean(eClass[:, 1])
+        return [x_mean, y_mean]
 
     # 2.5
-    def calculate_two_minimal_distances(self, func, ePoint, eClass):
+    @staticmethod
+    def calculate_two_minimal_distances(func, e_point, eClass):
         distances = [np.array([])]
         for point in eClass:
-            newDistance = func(ePoint, point)
-            distances = np.append(distances, newDistance)
+            new_distance = func(e_point, point)
+            distances = np.append(distances, new_distance)
         return sum(np.sort(distances)[:2])
 
 
@@ -68,73 +77,90 @@ class UI:
         self.func3_state = IntVar()
         self.func4_state = IntVar()
         self.func1 = Checkbutton(window, text='(object-centroid) euclid_distance', var=self.func1_state)
-        self.func2 = Checkbutton(window, text='(object-centroid) max_mod_distance', var=self.func2_state)
+        self.func2 = Checkbutton(window, text='(object-centroid) max_abs_val_distance', var=self.func2_state)
         self.func3 = Checkbutton(window, text='(object-object) euclid_distance', var=self.func3_state)
-        self.func4 = Checkbutton(window, text='(object-object) max_mod_distance', var=self.func4_state)
+        self.func4 = Checkbutton(window, text='(object-object) max_abs_val_distance', var=self.func4_state)
         self.func1.pack()
         self.func2.pack()
         self.func3.pack()
         self.func4.pack()
         self.plot()
 
-    def getColor(self, x, y):
+    def get_color(self, x, y):
         global result
         if x is not None and y is not None:
             calculations = Calculations()
-            classes = [class1, class3, class4, class6]
             colors = ['*r', '*g', '*b', '*y']
             distances = [np.array([])]
             if self.func1_state.get() == 1:
-                for c in classes:
+                for c in Data.classes:
                     d = calculations.calculate_distance_to_centroid(calculations.euclid_distance,
                                                                     [x, y], calculations.find_centroid(c))
                     distances = np.append(distances, d)
-                    result = np.where(distances == min(distances))
+                    result = np.where(distances == min(distances))[0][0]
+                print("Calculating distance from point to centroids (Euclid_distance)...")
                 print(distances)
-                print(result)
-                return colors[result[0][0]]
+                print(Data.naming)
+                print("New point goes to" + Data.naming[result])
+                Data.classes[result] = np.append(Data.classes[result], [[x, y]], axis=0)
+                return colors[result]
+
             elif self.func2_state.get() == 1:
-                for c in classes:
+                for c in Data.classes:
                     d = calculations.calculate_distance_to_centroid(calculations.max_mod_distance,
                                                                     [x, y], calculations.find_centroid(c))
                     distances = np.append(distances, d)
-                    result = np.where(distances == min(distances))
+                    result = np.where(distances == min(distances))[0][0]
+                print("Calculating distance from point to centroids (max_abs_val_distance)...")
                 print(distances)
-                print(result)
-                return colors[result[0][0]]
+                print(Data.naming)
+                print("New point goes to" + Data.naming[result])
+                Data.classes[result] = np.append(Data.classes[result], [[x, y]], axis=0)
+                return colors[result]
+
             elif self.func3_state.get() == 1:
-                for c in classes:
+                for c in Data.classes:
                     d = calculations.calculate_two_minimal_distances(calculations.euclid_distance,
                                                                      [x, y], c)
                     distances = np.append(distances, d)
-                    result = np.where(distances == min(distances))
+                    result = np.where(distances == min(distances))[0][0]
+                print("Calculating distance from point to objects (Euclid_distance)...")
                 print(distances)
-                print(result)
-                return colors[result[0][0]]
+                print(Data.naming)
+                print("New point goes to" + Data.naming[result])
+                Data.classes[result] = np.append(Data.classes[result], [[x, y]], axis=0)
+                return colors[result]
+
             elif self.func4_state.get() == 1:
-                for c in classes:
+                for c in Data.classes:
                     d = calculations.calculate_two_minimal_distances(calculations.max_mod_distance,
                                                                      [x, y], c)
                     distances = np.append(distances, d)
-                    result = np.where(distances == min(distances))
+                    result = np.where(distances == min(distances))[0][0]
+                print("Calculating distance from point to points (max_abs_val_distance)...")
                 print(distances)
-                print(result)
-                return colors[result[0][0]]
+                print(Data.naming)
+                print("New point goes to" + Data.naming[result])
+                Data.classes[result] = np.append(Data.classes[result], [[x, y]], axis=0)
+                return colors[result]
+
+            else:
+                return ''
 
     def plot(self):
         fig = Figure(figsize=(6, 6))
         plt = fig.add_subplot(111)
-        for i in enumerate(class1):
-            p1, = plt.plot(class1[:, 0], class1[:, 1], '*r')
-            p2, = plt.plot(class3[:, 0], class3[:, 1], '*g')
-            p3, = plt.plot(class4[:, 0], class4[:, 1], '*b')
-            p4, = plt.plot(class6[:, 0], class6[:, 1], '*y')
+        for i in enumerate(Data.class1):
+            p1, = plt.plot(Data.class1[:, 0], Data.class1[:, 1], '*r')
+            p2, = plt.plot(Data.class3[:, 0], Data.class3[:, 1], '*g')
+            p3, = plt.plot(Data.class4[:, 0], Data.class4[:, 1], '*b')
+            p4, = plt.plot(Data.class6[:, 0], Data.class6[:, 1], '*y')
 
         plt.legend([p1, p2, p3, p4], ["class1", "class3", "class4", "class6"])
         plt.grid(True)
 
         def onclick(event):
-            plt.plot(event.xdata, event.ydata, self.getColor(event.xdata, event.ydata))
+            plt.plot(event.xdata, event.ydata, self.get_color(event.xdata, event.ydata))
             fig.canvas.draw()
 
         canvas = FigureCanvasTkAgg(fig, master=self.window)
