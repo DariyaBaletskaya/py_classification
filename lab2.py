0e79473633c60d297a1e3429db342008f4c39890
Illia Uzun <you@example.com>
2019-09-26 21:18:46 +0300

    axis fix

--- lab2.py
+++ lab2.py
@@ -1,17 +1,15 @@
 import math as math
+import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 from tkinter import *
 from itertools import combinations
-import numpy as np
-
 
 BUTTON_ENABLE = 1
 
+
 # ------DATA-----#
-
-
-class Data:
+class Data():
     class1 = np.array([[0.49, 0.89],
                        [0.34, 0.81],
                        [0.36, 0.67],
@@ -37,7 +35,7 @@ class Data:
     naming = ["Class #1", "Class #2", "Class #3", "Class #4"]
 
 
-class Calculations:
+class Calculations(object):
     @staticmethod
     def euclid_distance(class1, class2):
         dx = (class1[0] - class2[0]) ** 2
@@ -81,7 +79,6 @@ class Calculations:
 
     @staticmethod
     def get_mid_section(first_centroid, second_centroid):
-
         x_o = (first_centroid[0] + second_centroid[0]) / 2
         y_o = (first_centroid[1] + second_centroid[1]) / 2
         return np.array([x_o, y_o])
@@ -98,7 +95,7 @@ class Calculations:
         }
 
 
-class UI:
+class UI(object):
     def __init__(self, window):
         self.window = window
         self.fig = Figure(figsize=(6, 6))
@@ -117,11 +114,10 @@ class UI:
         self.func4 = Checkbutton(
             window, text='(object-object) max_abs_val_distance', var=self.func4_state).pack()
 
-        self.plot()
         self.divide_classes(self.get_centroids())
+        self.plot()
 
     def get_color(self, x, y):
-        global result
         if x is not None and y is not None:
             calculations = Calculations()
             distances = [np.array([])]
@@ -188,12 +184,15 @@ class UI:
                 return ''
 
     def plot(self):
+        self.plt.set_xlim([0, 1])
+        self.plt.set_ylim([0, 1])
+
         p1, = self.plt.plot(Data.class1[:, 0], Data.class1[:, 1], '*r')
         p2, = self.plt.plot(Data.class3[:, 0], Data.class3[:, 1], '*g')
         p3, = self.plt.plot(Data.class4[:, 0], Data.class4[:, 1], '*b')
         p4, = self.plt.plot(Data.class6[:, 0], Data.class6[:, 1], '*y')
         self.plt.legend([p1, p2, p3, p4], [
-                        "class1", "class3", "class4", "class6"])
+            "class1", "class3", "class4", "class6"])
         self.plt.grid(True)
 
         def onclick(event):
@@ -213,7 +212,7 @@ class UI:
             centroids = np.append(centroids, centroid)
             color_index = np.where(Data.classes == c)[0][0]
             centroid_color = Data.colors[color_index][1]
-            self.plt.plot(centroid[0], centroid[1], "+"+centroid_color)
+            self.plt.plot(centroid[0], centroid[1], "+" + centroid_color)
         centroids = centroids.reshape(4, 2)
         return centroids
 
@@ -225,17 +224,14 @@ class UI:
             dx = first_centroid[0] - second_centroid[0]
             dy = first_centroid[1] - second_centroid[1]
             # connect them
-            self.plt.plot([first_centroid[0], second_centroid[0]], [
-                first_centroid[1], second_centroid[1]], '-r')
-            mid_point = Calculations.get_mid_section(
-                first_centroid, second_centroid)
+            self.plt.plot([first_centroid[0], second_centroid[0]],
+                          [first_centroid[1], second_centroid[1]], '-r')
+            mid_point = Calculations.get_mid_section(first_centroid, second_centroid)
             # plot center point
             self.plt.plot(mid_point[0], mid_point[1], 'og')
-            separating_line = Calculations.get_separating_line(
-                dx, dy, mid_point)
+            separating_line = Calculations.get_separating_line(dx, dy, mid_point)
             # plot perpendicular
-            self.plt.plot(separating_line['coord_x'],
-                          separating_line['coord_y'])
+            self.plt.plot(separating_line['coord_x'], separating_line['coord_y'])
 
 
 window = Tk()
