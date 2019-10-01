6c6e96943c0d87ed9b9f24ac3f62f8de60e6af6a
Illia Uzun <you@example.com>
2019-10-02 01:09:27 +0300

    line equations

--- lab2.py
+++ lab2.py
@@ -7,6 +7,7 @@ from itertools import combinations
 
 BUTTON_ENABLE = 1
 
+
 # ------DATA-----#
 class Data(object):
     class1 = np.array([[0.49, 0.89], [0.34, 0.81], [0.36, 0.67], [0.47, 0.49], [0.52, 0.53]])
@@ -17,9 +18,10 @@ class Data(object):
     classes = [class1, class3, class4, class6]
     colors = ['*r', '*g', '*b', '*y']
     naming = ["Class #1", "Class #2", "Class #3", "Class #4"]
+    centroids = np.array([[]])
 
 
-class Distance_Calculations(object):
+class Calculations(object):
     @staticmethod
     def euclid_distance(class1, class2):
         dx = (class1[0] - class2[0]) ** 2
@@ -71,24 +73,10 @@ class Distance_Calculations(object):
         coord_x_array = np.arange(0.01, 1, 0.1)
         return {
             'coord_x': coord_x_array,
-            'coord_y': Distance_Calculations.line_equation(a, b, coord_x_array),
+            'coord_y': Calculations.line_equation(a, b, coord_x_array),
             'a': a,
             'b': b
         }
-
-
-class Perpendiculars(object):
-
-    @staticmethod
-    def get_area(lines, centroid):
-        for line in lines:
-            d = -centroid[1] + line['a'] * centroid[0] + line['b']
-            print(d)
-        print("\n")
-    #
-    # @staticmethod
-    # def
-
 
 class UI(object):
     def __init__(self, window):
@@ -110,7 +98,7 @@ class UI(object):
 
     def get_color(self, x, y):
         if x is not None and y is not None:
-            calculations = Distance_Calculations()
+            calculations = Calculations()
             distances = [np.array([])]
             if self.func1_state.get() == BUTTON_ENABLE:
                 for c in Data.classes:
@@ -180,7 +168,7 @@ class UI(object):
         self.plt.legend([p1, p2, p3, p4], ["class1", "class3", "class4", "class6"])
         self.plt.grid(True)
 
-        def onclick(self, event):
+        def onclick(event):
             self.plt.plot(event.xdata, event.ydata, self.get_color(event.xdata, event.ydata))
             self.fig.canvas.draw()
 
@@ -192,7 +180,7 @@ class UI(object):
     def get_centroids(self):
         centroids = np.array([])
         for c in Data.classes:
-            centroid = Distance_Calculations.find_centroid(c)
+            centroid = Calculations.find_centroid(c)
             centroids = np.append(centroids, centroid)
             color_index = np.where(Data.classes == c)[0][0]
             centroid_color = Data.colors[color_index][1]
@@ -211,16 +199,96 @@ class UI(object):
             dy = first_centroid[1] - second_centroid[1]
             # connect them
             self.plt.plot([first_centroid[0], second_centroid[0]], [first_centroid[1], second_centroid[1]], '--r')
-            mid_point = Distance_Calculations.get_mid_section(first_centroid, second_centroid)
+            mid_point = Calculations.get_mid_section(first_centroid, second_centroid)
             # plot center point
             self.plt.plot(mid_point[0], mid_point[1], 'og')
-            separating_line = Distance_Calculations.get_separating_line(dx, dy, mid_point)
+            separating_line = Calculations.get_separating_line(dx, dy, mid_point)
             lines = np.append(lines, separating_line)
             # plot perpendicular
             self.plt.plot(separating_line['coord_x'], separating_line['coord_y'], ":k")
 
         for centroid in centroids:
-            Perpendiculars.get_area(lines, centroid)
+            Data.centroids = np.append(Data.centroids, [[centroid[0]], [centroid[1]]])
+        print(centroids)
+        # print(Perpendiculars.isClass1(Perpendiculars.center3))
+
+        print([Perpendiculars.isClass1(p) for p in Data.classes[0]])
+        print([Perpendiculars.isClass1(p) for p in Data.classes[1]])
+        print([Perpendiculars.isClass1(p) for p in Data.classes[2]])
+        print([Perpendiculars.isClass1(p) for p in Data.classes[3]])
+
+
+class Perpendiculars(object):
+    def isClass1(point):  # Принадлежит ли точка классу 1
+        center1 = [Data.centroids[0],Data.centroids[1]]
+        center2 = [Data.centroids[2],Data.centroids[3]]
+        center3 = [Data.centroids[4],Data.centroids[5]]
+        center4 = [Data.centroids[6],Data.centroids[7]]
+
+        dx_12 = center2[0] - center1[0]  # red-blue dx
+        dy_12 = center2[1] - center1[1]  # red-blue dy
+        dx_13 = center3[0] - center1[0]  # red-yellow dx
+        dy_13 = center3[1] - center1[1]  # red-yellow dy
+        dx_14 = center4[0] - center1[0]  # red-green dx
+        dy_14 = center4[1] - center1[1]  # red-green dy
+        dx_23 = center3[0] - center2[0]  # blue-yellow dx
+        dy_23 = center3[1] - center2[1]  # blue-yellow dy
+        dx_42 = center2[0] - center4[0]  # green-blue dx
+        dy_42 = center2[1] - center4[1]  # green-blue dy
+        dx_43 = center3[0] - center4[0]  # green-yellow dx
+        dy_43 = center3[1] - center4[1]  # green-yellow dy
+
+        mid_line_p_12 = Calculations.get_mid_section(center1, center2)
+        mid_line_p_13 = Calculations.get_mid_section(center1, center3)
+        mid_line_p_14 = Calculations.get_mid_section(center1, center4)
+        mid_line_p_23 = Calculations.get_mid_section(center2, center3)
+        mid_line_p_42 = Calculations.get_mid_section(center4, center4)
+        mid_line_p_43 = Calculations.get_mid_section(center4, center3)
+
+        sep_line_12 = Calculations.get_separating_line(dx_12, dy_12, mid_line_p_12)
+        sep_line_13 = Calculations.get_separating_line(dx_13, dy_13, mid_line_p_13)
+        sep_line_14 = Calculations.get_separating_line(dx_14, dy_14, mid_line_p_14)
+        sep_line_23 = Calculations.get_separating_line(dx_23, dy_23, mid_line_p_23)
+        sep_line_42 = Calculations.get_separating_line(dx_42, dy_42, mid_line_p_42)
+        sep_line_43 = Calculations.get_separating_line(dx_43, dy_43, mid_line_p_43)
+        sep_lines = [[x['a'], x['b']] for x in
+                     [sep_line_12, sep_line_13, sep_line_14, sep_line_23, sep_line_42,
+                      sep_line_43]]
+
+        # находим коэффициенты разделяющих линий для класса 1
+        sep_line_k_12 = [sep_line_12['a'], sep_line_12['b']]
+        sep_line_k_13 = [sep_line_13['a'], sep_line_13['b']]
+        sep_line_k_14 = [sep_line_14['a'], sep_line_14['b']]
+        # находим решающие функции для класса 1
+        d12 = point[1] - sep_line_k_12[0] * point[0] - sep_line_k_12[1]
+        d13 = point[1] - sep_line_k_13[0] * point[0] - sep_line_k_13[1]
+        d14 = point[1] - sep_line_k_14[0] * point[0] - sep_line_k_14[1]
+        return d12 > 0 and d13 > 0 and d14 > 0
+
+    def isClass2(point):  # Принадлежит ли точка классу 2
+        sep_line_k_12 = [Perpendiculars.sep_line_12['a'], Perpendiculars.sep_line_12['b']]
+        sep_line_k_23 = [Perpendiculars.sep_line_23['a'], Perpendiculars.sep_line_23['b']]
+        sep_line_k_24 = [-Perpendiculars.sep_line_42['a'], -Perpendiculars.sep_line_42['b']]
+        d12 = point[1] - sep_line_k_12[0] * point[0] - sep_line_k_12[1]
+        d23 = point[1] - sep_line_k_23[0] * point[0] - sep_line_k_23[1]
+        d24 = point[1] - sep_line_k_24[0] * point[0] - sep_line_k_24[1]
+        return d12 <= 0 and d23 > 0 and d24 > 0
+
+    def isClass3(point):  # FIX
+        sep_line_k_13 = [Perpendiculars.sep_line_13['a'], Perpendiculars.sep_line_13['b']]
+        sep_line_k_23 = [Perpendiculars.sep_line_23['a'], Perpendiculars.sep_line_23['b']]
+        sep_line_k_34 = [-Perpendiculars.sep_line_43['a'], -Perpendiculars.sep_line_43['b']]
+        d13 = point[1] - sep_line_k_13[0] * point[0] - sep_line_k_13[1]
+        d23 = point[1] - sep_line_k_23[0] * point[0] - sep_line_k_23[1]
+        d34 = point[1] - sep_line_k_34[0] * point[0] - sep_line_k_34[1]
+        return d13 <= 0 and d23 <= 0 and d34 <= 0
+
+    @staticmethod
+    def get_area(lines, centroid):
+        for line in lines:
+            d = -centroid[1] + line['a'] * centroid[0] + line['b']
+            print(d)
+        print("\n")
 
 
 window = Tk()
