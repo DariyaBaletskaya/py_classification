72874eced807bf0f72ea98442ed0b97031ea158c
Dariya Baletskaya <baletskaya.dariya@gmail.com>
2019-11-03 00:19:59 +0200

    refactor function names, add train function

--- lab5.py
+++ lab5.py
@@ -27,14 +27,18 @@ def get_weight_matrix(matrix):
     matrix_flatten = np.reshape(matrix, (63, 1))
     matrix_flatten_transpose = matrix_flatten.transpose()
     weight_matrix = matrix_flatten.dot(matrix_flatten_transpose)
-    np.fill_diagonal(weight_matrix, 0)
 
     return weight_matrix
 
 
-def sum_matrix(first, second):
+def get_flatten_matrix(matrix):
+    return np.reshape(matrix, (63, 1)).transpose()
+
+
+def get_feature_matrix(first, second):
     result = np.array([[first[i][j] + second[i][j]
                         for j in range(len(first[0]))] for i in range(len(first))])
+    np.fill_diagonal(result, 0)
     return result
 
 
@@ -45,9 +49,20 @@ def plot_letter(figure, matrix, plot_position):
     plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.ocean)
 
 
+def train(matrix_flatten, feature_matrix):
+    result = matrix_flatten.dot(feature_matrix)
+    result = np.sign(result)
+    result = np.reshape(result, (9, 7))
+    return result
+
+
 # plotting
 fig = plt.figure()
 plot_letter(fig, D_MATRIX, [2, 3, 1])
-plot_letter(fig, B_MATRIX, [2, 2, 2])
-print(sum_matrix(get_weight_matrix(D_MATRIX), get_weight_matrix(B_MATRIX)))
+# plot_letter(fig, B_MATRIX, [2, 2, 2])
+
+plot_letter(fig, train(get_flatten_matrix(D_MATRIX), get_feature_matrix(
+    get_weight_matrix(D_MATRIX), get_weight_matrix(B_MATRIX))), [2, 2, 2])
+print(train(get_flatten_matrix(D_MATRIX), get_feature_matrix(
+    get_weight_matrix(D_MATRIX), get_weight_matrix(B_MATRIX))))
 plt.show()
